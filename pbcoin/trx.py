73300421e9fbd0af709af13ac74ff82c08e87408
Mahdi <esmokes17@gmail.com>
2022-07-01 12:06:06 +0430

    better trx class

--- pbcoin/trx.py
+++ pbcoin/trx.py
@@ -1,19 +1,57 @@
+from datetime import datetime
+from hashlib import sha512
+import json
+
 DEFAULT_SUBSIDY = 50
 
 class Trx:
     sender: str
     recipient: str
     amount: float
-    def __init__(self, *args):
-        if len(args) == 0:
+    time: float
+    hashTrx: str
+    def __init__(self, **kwargs):
+        if len(kwargs) == 0:
             # this is subsidy trx
-            #TODO
             self.sender = ""
-            self.recipient = "self"
+            self.recipient = "self" # TODO: get address key
             self.amount = DEFAULT_SUBSIDY
-        elif len(args) == 3:
-            self.sender = args[0]
-            self.recipient = args[1]
-            self.amount = args[3]
+            self.time = datetime.utcnow().timestamp()
+        elif len(kwargs) == 3:
+            self.sender = kwargs['sender']
+            self.recipient = kwargs['recipient']
+            self.amount = kwargs['amount']
+            if not kwargs.get('time'):
+                self.time = kwargs['time']
+            else:
+                self.time = datetime.utcnow().timestamp()
         else:
-            assert (False, "bad args")
+            assert (False, "Bad usage")
+
+        self.hashTrx = self.__hash__()
+    
+    def calculateHash(self):
+        calHash = sha512(self.__str__()).hexdigest()
+        self.blockHash = calHash
+        return calHash
+
+    def getData(self, with_hash: False, is_POSIX_timestamp = True):
+        data = {
+            'sender': self.sender,
+            'recipient': self.recipient,
+            'amount': self.amount,
+            'time': self.time if is_POSIX_timestamp else datetime.fromtimestamp(self.time)
+        }
+        if with_hash:
+            data['hash'] = self.hashTrx
+        return json.dumps(data)
+
+    @property
+    def __hash__(self) -> str:
+        return sha512(self.__str__().encode()).hexdigest()
+
+    def __str__(self) -> str:
+        return f'{self.sender}{self.recipient}{self.amount}{self.time}'
+    
+    def __repr__(self) -> str:
+        return self.getData(with_hash=True, is_POSIX_timestamp=False)
