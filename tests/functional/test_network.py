757adafcc06dc91d16c27cbcc4d6bd528e9d99e6
Mahdi <esmokes17@gmail.com>
2023-02-22 19:08:30 +0330

    start writing a new unittest for new network structure with test connection

--- tests/functional/test_network.py
+++ tests/functional/test_network.py
@@ -1,19 +1,19 @@
 import asyncio
-import json
+from concurrent.futures import ProcessPoolExecutor
 
 import pytest
 
-from pbcoin.blockchain import BlockChain
 from pbcoin.constants import TOTAL_NUMBER_CONNECTIONS
-from pbcoin.mine import Mine
-from pbcoin.net import Node, ConnectionCode
-from pbcoin.wallet import Wallet
+from pbcoin.netbase import Addr, ConnectionCode, Message
+from pbcoin.network import Node
 
 
 class TestNetworkBase:
     BASE_IP = '127.0.0'
     SENDER_IP = f'{BASE_IP}.1'
     RECEIVER_IP = f'{BASE_IP}.2'
+    PORT = 8989
+    MAX_WORKERS = 6
 
     @pytest.fixture
     async def run_nodes(self, request):
@@ -24,25 +24,18 @@ class TestNetworkBase:
         """
         self.__class__.nodes = []
         for i in range(request.param):
-            blockchain = BlockChain([])
-            wallet = Wallet()
-            node = None
-            miner = Mine(blockchain, wallet, node, [])
-            node = Node(
-                blockchain,
-                wallet,
-                miner,
-                dict(),
-                f"127.0.0.{i+1}", 8989
-            )
+            addr = Addr(ip=f"{self.BASE_IP}.{i+1}",
+                        port=self.PORT,
+                        pub_key=f"0x2{i+1}")  # TODO: make a valid public key with Key class
+            node = Node(addr)
             self.nodes.append(node)
         self.__class__.tasks = []
         for node in self.nodes:
             task = asyncio.create_task(node.listen())
             self.tasks.append(task)
-            if node.ip == self.SENDER_IP:
+            if node.addr.ip == self.SENDER_IP:
                 continue
-            await node.start_up(["127.0.0.1:8989"], False)
+            await node.start_up([f"{self.SENDER_IP}:{self.PORT}"], False)
         
         def close_nodes():
             for node in self.nodes:
@@ -56,11 +49,11 @@ class TestNetworkBase:
 class TestMakeConnection(TestNetworkBase):
     @pytest.fixture
     def actual_neighbors(self):
-        def actual(ip):
+        def actual(addr):
             data = dict()
             for node in self.nodes:
-                if node.ip != ip:
-                    data[node.uid] = (node.ip, node.port)
+                if node.addr != addr:
+                    data[node.addr.pub_key] = node.addr
             return data
         return actual
 
@@ -73,25 +66,29 @@ class TestMakeConnection(TestNetworkBase):
                 actual_len_neighbors = ((len(self.nodes) - 1)
                                         if (len(self.nodes)-1) <= TOTAL_NUMBER_CONNECTIONS
                                         else TOTAL_NUMBER_CONNECTIONS)
-                assert len(node.neighbors) == actual_len_neighbors, f"{node.ip}"
+                assert len(node.neighbors) == actual_len_neighbors, f"{node.addr.hostname}"
                 # TODO: better check neighbors when neighbors is more than TOTAL_NUMBER_CONNECTIONS
-                assert node.neighbors.items() <= actual_neighbors(node.ip).items()
+                assert node.neighbors.items() <= actual_neighbors(node.addr).items()
         return check
 
     @pytest.mark.parametrize("run_nodes", [2], ids=["two nodes"], indirect=True)
     async def test_is_up_and_interactable(self, run_nodes):
         """run 2 nodes and check they connected or not"""
-        data = {
-            "type": ConnectionCode.PING,
-            "src_ip": self.SENDER_IP,
-            "dst_ip": self.RECEIVER_IP
-        }
         sender = self.nodes[0]
-        rec = await sender.connect_and_send(
-            self.RECEIVER_IP, 8989, json.dumps(data), wait_for_receive=True)
-        assert rec != None and rec != b'', "Bad response"
-        assert json.loads(rec.decode())["status"], "Could not response request"
+        receiver = self.nodes[1]
+        request = Message(True, ConnectionCode.PING, sender.addr)
+        try:
+            rec = await sender.connect_and_send(receiver.addr,
+                                                request.create_message(sender.addr),
+                                                wait_for_receive=True)
+            assert rec != None and rec != b'', "Bad response"
+            assert Message.from_str(rec.decode()).status, "Could not response request"
+        except ConnectionError:
+            assert False, "Could not connect to receiver"
+        except Exception:
+            assert False, "Bad response"
 
+    
     @pytest.mark.parametrize(
         "run_nodes", [2, 4], ids = ["two nodes", "four nodes"], indirect = True
     )
@@ -103,8 +100,6 @@ class TestMakeConnection(TestNetworkBase):
             actual_len_neighbors = ((len(self.nodes) - 1)
                                     if (len(self.nodes)-1) <= TOTAL_NUMBER_CONNECTIONS
                                     else TOTAL_NUMBER_CONNECTIONS)
-            assert len(node.neighbors) == actual_len_neighbors, f"{node.ip}"
+            assert len(node.neighbors) == actual_len_neighbors, f"{node.addr.hostname}"
             # TODO: better check neighbors when neighbors is more than TOTAL_NUMBER_CONNECTIONS
-            assert node.neighbors.items() <= actual_neighbors(node.ip).items()
-
-# TODO: test functionally message handling
+            assert node.neighbors.items() <= actual_neighbors(node.addr).items()
