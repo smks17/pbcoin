755043c894f484f5d3afb4dcdf0d83f854ea242b
Mahdi <esmokes17@gmail.com>
2023-03-02 22:37:56 +0330

    Add new parameters to ```class Wallet``` methods to proper for unittest

--- pbcoin/wallet.py
+++ pbcoin/wallet.py
@@ -2,13 +2,17 @@ from __future__ import annotations
 
 from os import makedirs
 from random import random
-from typing import Dict, List
+from typing import Dict, List, Optional, TYPE_CHECKING
 
 from ellipticcurve.privateKey import PrivateKey
 from ellipticcurve.signature import Signature
 from ellipticcurve.ecdsa import Ecdsa
 
 import pbcoin.config as conf
+if TYPE_CHECKING:
+    from pbcoin.blockchain import BlockChain
+    from pbcoin.mempool import Mempool
+    from pbcoin.net import Node
 
 from .trx import Trx, Coin
 import pbcoin.core as core
@@ -59,25 +63,37 @@ class Wallet:
                         self.out_coins[out_coin.trx_hash] = [out_coin]
                     self.n_amount += out_coin.value
 
-    async def send_coin(self, recipient: str, value: float) -> bool:
+    async def send_coin(self,
+                        recipient: str,
+                        value: float,
+                        mempool: Optional[Mempool],  # just uses for unittest
+                        blockchain: Optional[BlockChain],  # just uses for unittest
+                        node: Optional[Node]  # just uses for unittest
+    ) -> bool:
         """make a transaction to send coins and publish trx to the network"""
+        if mempool is None:
+            mempool = core.MEMPOOL
+        if blockchain is None:
+            blockchain = core.BLOCK_CHAIN
+        if node is None:
+            blockchain = core.NETWORK
         # if user have amount for sending
         if value <= self.n_amount:
             made_trx = Trx.make_trx(sum(list(self.out_coins.values()), []),
                                         self.public_key, recipient, value)
             # add to own mempool
-            if not core.MEMPOOL.add_new_transaction(made_trx, self.sign(made_trx), core.BLOCK_CHAIN.last_block, self.walletKey.publicKey()):
+            if not mempool.add_new_transaction(made_trx, self.sign(made_trx), blockchain.last_block, self.walletKey.publicKey()):
                 return False
             # send to nodes and add to network mempool
             if conf.settings.glob.network:
-                await core.NETWORK.send_new_trx(made_trx)
+                await node.send_new_trx(made_trx)
             return True
         else:
             return False
 
-    def sign(self, trx_) -> Signature:
+    def sign(self, trx: Trx) -> Signature:
         """sign the transaction for add to mempool or send other nodes"""
-        return Ecdsa.sign(trx_.__hash__, core.WALLET.walletKey)
+        return Ecdsa.sign(trx.__hash__, self.walletKey)
 
     def base64Sign(self, trx_) -> bytes:
         """sign data and return base 64 signature"""
